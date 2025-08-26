78f220f8de2b141ece68b0ad7e7572e8009a3d50
tmdtky <tmdtky@gmail.com>
2025-08-26 10:56:11 +0900

    管理者画面へのアクセス制限追加

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,12 +2,15 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useRouteGuard } from './_hooks/useRouteGuard'
 
 export default function AdminLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	useRouteGuard()
+
 	const pathname = usePathname()
 	const isSelected = (href: string) => {
 		return pathname.includes(href)
