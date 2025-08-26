56d3d740e7e65976265a1fa8ea2bf5448bacbabf
tmdtky <tmdtky@gmail.com>
2025-08-26 10:44:09 +0900

    ログイン状態の取得及びヘッダー表示の出し分け追加

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,17 +1,44 @@
 "use client";
 
+import Link from 'next/link'
 import React from "react";
-import Link from "next/link";
+import { useSupabaseSession } from '../_hooks/useSupabaseSession';
+import { supabase } from '@/utils/supabase'
 
 export const Header: React.FC = () => {
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    window.location.href = '/'
+  }
+
+  const { session, isLoading } = useSupabaseSession()
+
   return (
-    <header className="bg-gray-800 text-white px-6 py-6 font-bold flex justify-between items-center">
-      <Link href="/" className="text-white no-underline hover:text-gray-300">
+    <header className="bg-gray-800 text-white p-6 font-bold flex justify-between items-center">
+      <Link href="/" className="header-link">
         Blog
       </Link>
-      <Link href="/contact" className="text-white no-underline hover:text-gray-300">
-        お問い合わせ
-      </Link>
+      {!isLoading && (
+        <div className="flex items-center gap-4">
+          {session ? (
+            <>
+              <Link href="/admin" className="header-link">
+                管理画面
+              </Link>
+              <button onClick={handleLogout}>ログアウト</button>
+            </>
+          ) : (
+            <>
+              <Link href="/contact" className="header-link">
+                お問い合わせ
+              </Link>
+              <Link href="/login" className="header-link">
+                ログイン
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </header>
   );
 };
